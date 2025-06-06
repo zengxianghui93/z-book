baea35439c1ae27b0e3cd697e877727dda6f631d
Zoro <869677271@qq.com>
2025-06-06 10:24:13 +0800

    init

---
+++ app.vue
@@ -0,0 +1,11 @@
+<!--
+ * @Description: 
+ * @Version: 1.0
+ * @Author: zengxianghui
+ * @Date: 2025-06-04 12:13:51
+ * @LastEditors: zengxianghui
+ * @LastEditTime: 2025-06-06 10:13:08
+-->
+<template>
+  <NuxtPage />
+</template>
---
+++ pages/index.vue
@@ -0,0 +1,14 @@
+<!--
+ * @Description: 
+ * @Version: 1.0
+ * @Author: zengxianghui
+ * @Date: 2025-06-06 10:16:37
+ * @LastEditors: zengxianghui
+ * @LastEditTime: 2025-06-06 10:16:45
+-->
+<script setup lang="ts"></script>
+<template>
+  <div>
+    <h1>Welcome</h1>
+  </div>
+</template>
